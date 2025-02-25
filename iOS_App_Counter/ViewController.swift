a03d989bf6e95b916bba8375e57140db3a2a97a7
NH <mnh4140@gmail.com>
2025-02-25 15:51:49 +0900

    카운터 앱 기능 구현

--- iOS_App_Counter/ViewController.swift
+++ iOS_App_Counter/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myNumber: UILabel!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var restetButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapPlusButton(_ sender: Any) {
+        var count: Int = Int(myNumber.text!) ?? 0
+        
+        if count < 10 {
+            minusButton.isEnabled = true
+            
+            count += 1
+            myNumber.text = "\(count)"
+            if count == 10 {
+                plusButton.isEnabled = false
+            }
+        }
+    }
+    
+    @IBAction func didTapMinusButton(_ sender: Any) {
+        var count: Int = Int(myNumber.text!) ?? 0
+        
+        if count > -10 {
+            plusButton.isEnabled = true
+            
+            count -= 1
+            myNumber.text = "\(count)"
+            
+            if count == -10 {
+                minusButton.isEnabled = false
+            }
+        }
+    }
+    
+    @IBAction func didTapRestButton(_ sender: Any) {
+        myNumber.text = "0"
+        plusButton.isEnabled = true
+        minusButton.isEnabled = true
+    }
 }
 
