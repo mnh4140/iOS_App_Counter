67ef472baa29799a6cc722afb6eed5ea39160648
NH <mnh4140@gmail.com>
2025-02-25 14:45:34 +0900

    Initial Commit

---
+++ iOS_App_CounterTests/iOS_App_CounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_App_CounterTests.swift
+//  iOS_App_CounterTests
+//
+//  Created by NH on 2/25/25.
+//
+
+import Testing
+@testable import iOS_App_Counter
+
+struct iOS_App_CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
